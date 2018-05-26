564b48aa98bb4b602e1d04266787951744a48765
eykamp <chris@eykamp.com>
2018-05-25 21:40:39 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deq_tools',
+      version='0.1',
+      description='Tools for downloading Oregon DEQ Air Quality data',
+      url='https://github.com/eykamp/deq_tools',
+      author='Chris Eykamp',
+      author_email='chris@eykamp.com',
+      license='MIT',
+      packages=['deq_tools'],
+      zip_safe=False)
