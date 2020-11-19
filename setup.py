7d68a83b2933c88a24dc3462ca67b463ba99d067
eykamp <chris@eykamp.com>
2020-11-19 02:45:48 -0800

    Notes

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@
 # Create new release on GitHub
 # update this file with new version and download_url
 # Run the following:
-#    python setup.py sdist
-#    python setup.py sdist upload
+#    python setup.py sdist bdist_wheel
+#    twine check dist/*
+# check SOURCES.txt to make sure no extraneous files were included
+#    python -m twine upload  dist/*
 #    del /q deq_tools.egg-info
 #    del /q dist
+#    del /q build
 
 import setuptools
 
@@ -13,9 +16,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    download_url="https://github.com/eykamp/deq_tools/archive/v0.2.0.tar.gz",
-    version="0.2.0",
 	name="deq_tools",
+    version="0.2.0",
+    download_url="https://github.com/eykamp/deq_tools/archive/v0.2.0.tar.gz",
     author="Chris Eykamp",
     author_email="chris@eykamp.com",
     description="Tools for downloading Oregon DEQ Air Quality data",
@@ -29,10 +32,5 @@ setuptools.setup(
       	"Programming Language :: Python :: 3",
       	"License :: OSI Approved :: MIT License",
       	"Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "requests",
-        "pydantic",
-        "tenacity",
-    ],
+    ]
 )
