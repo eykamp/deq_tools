fe7c76b7cefdeac5ccbd6ad6f345201eac51a3b4
eykamp <chris@eykamp.com>
2020-11-19 01:25:41 -0800

    Changes to publishing script

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    download_url='https://github.com/eykamp/deq_tools/archive/v0.1.4.tar.gz',
-    version='0.1.4',
+    download_url='https://github.com/eykamp/deq_tools/archive/v0.2.0.tar.gz',
+    version='0.2.0',
 	name='deq_tools',
     author='Chris Eykamp',
     author_email='chris@eykamp.com',
